a33e0890ecc130b8ae58ee4b79146025aa5ca851
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-10 16:50:37 -0300

    Update WorkshopModel.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/model/WorkshopModel.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/model/WorkshopModel.java
@@ -1,10 +1,11 @@
 package com.enzo.yxzapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "workshops")
@@ -12,4 +13,51 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class WorkshopModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String school;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WorkshopType type;
+
+    @Column(name = "school_contact", nullable = false)
+    private String schoolContact;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Segment segment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Shift shift;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ClassGroup classGroup;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WorkshopStatus status = WorkshopStatus.AGENDADA;
+
+
+
+
+
+
+
+
+
+
+
 }
