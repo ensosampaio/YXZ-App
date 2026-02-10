f08ca911e255c3f06434c7d7a939204bdc994360
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-10 15:53:25 -0300

    First Commit

---
+++ YXZ-App/src/main/java/com/enzo/yxzapp/YxzAppApplication.java
@@ -0,0 +1,13 @@
+package com.enzo.yxzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxzAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxzAppApplication.class, args);
+    }
+
+}
