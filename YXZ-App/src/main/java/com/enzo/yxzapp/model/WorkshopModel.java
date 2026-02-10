76856929d43fd42bd6ab856c7abe3948619f0485
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-10 16:45:30 -0300

    Update WorkshopModel.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/model/WorkshopModel.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/model/WorkshopModel.java
@@ -1,4 +1,15 @@
 package com.enzo.yxzapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "workshops")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkshopModel {
 }
