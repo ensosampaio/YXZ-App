38d2d08f589cbb3241576abcf0e9d3da1abce988
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:48:41 -0300

    Update UpdateViaModalRequest

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/oficina/UpdateViaModalRequest.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/oficina/UpdateViaModalRequest.java
@@ -1,4 +1,15 @@
 package com.enzo.yxzapp.dto.oficina;
 
-public class UpdateViaModalRequest {
-}
+import com.enzo.yxzapp.enums.StatusOficina;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+import java.util.List;
+
+public record UpdateViaModalRequest(
+        StatusOficina status,
+        List<String> instrutores,
+        @Min(1) @Max(10) Integer avaliacaoEscola,
+        Integer quantitativoAluno,
+        String acompanhanteTurma
+) {}
