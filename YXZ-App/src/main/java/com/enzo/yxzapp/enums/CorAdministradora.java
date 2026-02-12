d32af7405474edb5e3e8dcfbe936f20d432a1b92
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:05:55 -0300

    Update CorAdministradora.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/enums/CorAdministradora.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/enums/CorAdministradora.java
@@ -1,23 +1,8 @@
 package com.enzo.yxzapp.enums;
 
 public enum CorAdministradora {
-    ROSA("#FFB3D9", "#FFF0F7"),
-    AZUL_CEU("#87CEEB", "#E6F7FF"),
-    AMBAR("#FFBF00", "#FFF8E1");
-
-    private final String corPrincipal;
-    private final String corSuave;
-
-    CorAdministradora(String corPrincipal, String corSuave) {
-        this.corPrincipal = corPrincipal;
-        this.corSuave = corSuave;
-    }
-
-    public String getCorPrincipal() {
-        return corPrincipal;
-    }
-
-    public String getCorSuave() {
-        return corSuave;
-    }
+    ROSA,
+    AZUL_CEU,
+    AMBAR,
+    ROXO_ROOT
 }
