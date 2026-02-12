9736ce0074cd03ba0bb28a3b5aded0e92639107e
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 04:11:05 -0300

    Update AuthController.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/auth/AuthController.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/auth/AuthController.java
@@ -1,4 +1,25 @@
 package com.enzo.yxzapp.auth;
 
+import com.enzo.yxzapp.dto.auth.LoginRequest;
+import com.enzo.yxzapp.dto.auth.LoginResponse;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody @Valid LoginRequest req) {
+        return authService.login(req);
+    }
 }
