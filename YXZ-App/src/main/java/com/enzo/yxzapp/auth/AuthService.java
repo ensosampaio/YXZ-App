7de24b32490b820c15d7908142d6023f6e292755
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 04:12:26 -0300

    Update AuthService.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/auth/AuthService.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/auth/AuthService.java
@@ -1,4 +1,8 @@
 package com.enzo.yxzapp.auth;
 
+import com.enzo.yxzapp.dto.auth.LoginRequest;
+import com.enzo.yxzapp.dto.auth.LoginResponse;
+
 public interface AuthService {
+    LoginResponse login(LoginRequest loginRequest);
 }
