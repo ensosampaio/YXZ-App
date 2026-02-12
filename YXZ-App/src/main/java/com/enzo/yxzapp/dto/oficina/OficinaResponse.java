ab81098626df0a36820bad35326d3e5aa419bd8f
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:49:43 -0300

    Update OficinaResponse.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/oficina/OficinaResponse.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/oficina/OficinaResponse.java
@@ -1,4 +1,29 @@
 package com.enzo.yxzapp.dto.oficina;
 
-public class OficinaResponse {
-}
+import com.enzo.yxzapp.enums.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OficinaResponse(
+        Long id,
+        String escola,
+        String cidade,
+        LocalDate data,
+        TipoOficina tipo,
+        String contatoEscola,
+        Segmento segmento,
+        Turno turno,
+        Turma turma,
+        StatusOficina status,
+        List<String> instrutores,
+        Integer avaliacaoEscola,
+        Integer quantitativoAluno,   // pode ser null
+        String acompanhanteTurma,    // pode ser null
+        String criadorNome,
+        CorAdministradora corCriador,
+        LocalDateTime dataCriacao,
+        String ultimoAtualizadorNome,
+        LocalDateTime dataAtualizacao
+) {}
