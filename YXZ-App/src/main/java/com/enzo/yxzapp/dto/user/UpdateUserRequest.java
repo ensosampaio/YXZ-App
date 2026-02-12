178899bc2fd3577ccdfd32c4e207a428634c4d59
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:43:18 -0300

    Update UpdateUserRequest.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/user/UpdateUserRequest.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/user/UpdateUserRequest.java
@@ -1,4 +1,11 @@
 package com.enzo.yxzapp.dto.user;
 
-public class UpdateUserRequest {
-}
+import com.enzo.yxzapp.enums.CorAdministradora;
+import com.enzo.yxzapp.enums.Role;
+
+public record UpdateUserRequest(
+        String nome,
+        Role role,
+        CorAdministradora corAdministradora,
+        Boolean ativo
+) {}
