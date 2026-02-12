23e75ad31607b7cf08147bcf7bdc623a0a22cb09
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:11:57 -0300

    Update UserRepository.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/repository/UserRepository.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.enzo.yxzapp.repository;
 
-public interface UserRepository {
+import com.enzo.yxzapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
