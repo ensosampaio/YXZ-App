c2e2b16af8864754050f51b77569b85c7220288c
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:35:28 -0300

    Update ApiError.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/exception/ApiError.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/exception/ApiError.java
@@ -1,4 +1,11 @@
 package com.enzo.yxzapp.exception;
 
-public class ApiError {
-}
+import java.time.Instant;
+
+public record ApiError(
+        Instant timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {}
