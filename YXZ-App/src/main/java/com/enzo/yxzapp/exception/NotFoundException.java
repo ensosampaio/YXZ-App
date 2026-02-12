615bdf3f60374669e1e490270db5ed0e05bcbc37
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:32:57 -0300

    Update NotFoundException.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/exception/NotFoundException.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/exception/NotFoundException.java
@@ -1,4 +1,5 @@
 package com.enzo.yxzapp.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message); }
 }
