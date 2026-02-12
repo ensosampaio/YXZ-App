13c2db93803c506a191aea6c175e183b92e187b2
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:54:37 -0300

    Update SecurityConfig.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/security/SecurityConfig.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.enzo.yxzapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,9 +21,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ROOT")
                         .requestMatchers("/oficinas/**", "/calendar/**").authenticated()
