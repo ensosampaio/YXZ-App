030f5ad8c5fa25a294beb33c36f349f56379b469
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:44:07 -0300

    Update UserResponse.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/user/UserResponse.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/user/UserResponse.java
@@ -1,4 +1,17 @@
 package com.enzo.yxzapp.dto.user;
 
-public class UserResponse {
-}
+import com.enzo.yxzapp.enums.CorAdministradora;
+import com.enzo.yxzapp.enums.Role;
+
+import java.time.LocalDateTime;
+
+public record UserResponse(
+        Long id,
+        String nome,
+        String email,
+        Role role,
+        CorAdministradora corAdministradora,
+        boolean ativo,
+        LocalDateTime criadoEm,
+        LocalDateTime atualizadoEm
+) {}
