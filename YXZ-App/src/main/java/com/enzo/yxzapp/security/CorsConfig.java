207ec11df5246823cd470d8478584665c692458a
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:31:03 -0300

    Update CorsConfig.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/security/CorsConfig.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/security/CorsConfig.java
@@ -1,4 +1,27 @@
 package com.enzo.yxzapp.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        var config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        config.setAllowCredentials(true);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
