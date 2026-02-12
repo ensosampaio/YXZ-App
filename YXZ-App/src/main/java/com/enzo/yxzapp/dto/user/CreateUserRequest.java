a2708cbef51f5d545265bdbe92cb736d964a9211
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:42:36 -0300

    Update CreateUserRequest.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/user/CreateUserRequest.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/user/CreateUserRequest.java
@@ -1,4 +1,15 @@
 package com.enzo.yxzapp.dto.user;
 
-public class CreateUserRequest {
-}
+import com.enzo.yxzapp.enums.CorAdministradora;
+import com.enzo.yxzapp.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateUserRequest(
+        @NotBlank String nome,
+        @Email @NotBlank String email,
+        @NotBlank String senha,
+        @NotNull Role role,
+        CorAdministradora corAdministradora
+) {}
