bb8edbf77a286a86fdca1d02f6f5b54854bce93c
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:47:01 -0300

    Update CreateOficinaRequest.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/oficina/CreateOficinaRequest.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/oficina/CreateOficinaRequest.java
@@ -1,4 +1,21 @@
 package com.enzo.yxzapp.dto.oficina;
 
-public class CreateOficinaRequest {
-}
+import com.enzo.yxzapp.enums.Segmento;
+import com.enzo.yxzapp.enums.TipoOficina;
+import com.enzo.yxzapp.enums.Turma;
+import com.enzo.yxzapp.enums.Turno;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record CreateOficinaRequest(
+        @NotBlank String escola,
+        @NotBlank String cidade,
+        @NotNull LocalDate data,
+        @NotNull TipoOficina tipo,
+        @NotBlank String contatoEscola,
+        @NotNull Segmento segmento,
+        @NotNull Turno turno,
+        @NotNull Turma turma
+) {}
