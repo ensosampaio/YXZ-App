5dd3adb0bc199c59ac9c4358219c34e86c80714a
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 03:33:32 -0300

    Update BadRequestException.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/exception/BadRequestException.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/exception/BadRequestException.java
@@ -1,4 +1,5 @@
 package com.enzo.yxzapp.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) { super(message); }
 }
