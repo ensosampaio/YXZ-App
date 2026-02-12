d5a7f01612a6ff2dcc84c001d6c963c7013492f9
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 02:42:57 -0300

    Update OficinaService.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/service/OficinaService.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/service/OficinaService.java
@@ -1,4 +1,15 @@
 package com.enzo.yxzapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OficinaService {
+
+    @Autowired
+    private OficinaRepository oficinaRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
 }
