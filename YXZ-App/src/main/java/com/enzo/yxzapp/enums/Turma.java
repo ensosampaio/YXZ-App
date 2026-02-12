72e09ac40beb556d99bde52e0d6b28a2b6a9f15f
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-12 02:38:11 -0300

    Update Turma

--- YXZ-App/src/main/java/com/enzo/yxzapp/enums/Turma.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/enums/Turma.java
@@ -1,5 +1,14 @@
 package com.enzo.yxzapp.enums;
 
 public enum Turma {
-    
+    _6ANO,
+    _7ANO,
+    _8ANO,
+    _9ANO,
+    _1ANOEM,
+    _2ANOEM,
+    _3ANOEM,
+    EJA,
+    TECNICO,
+    SUPERIOR
 }
