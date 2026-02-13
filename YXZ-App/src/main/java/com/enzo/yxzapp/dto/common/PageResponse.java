4cbe7a1a2e352fe1cfad4c1f430d8021096fc7bc
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-13 01:51:48 -0300

    Update PageResponse.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/dto/common/PageResponse.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/dto/common/PageResponse.java
@@ -1,6 +1,9 @@
 package com.enzo.yxzapp.dto.common;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
+import java.util.function.Function;
 
 public record PageResponse<T>(
         List<T> items,
@@ -8,4 +11,9 @@ public record PageResponse<T>(
         int totalPages,
         int page,
         int size
-) {}
+) {
+    public static <E, D> PageResponse<D> fromPage(Page<E> page, Function<E, D> mapper) {
+        List<D> items = page.getContent().stream().map(mapper).toList();
+        return new PageResponse<>(items, page.getTotalElements(), page.getTotalPages(), page.getNumber(), page.getSize());
+    }
+}
