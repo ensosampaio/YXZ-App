1aae832f257a96a0419fb48066c20306f452d246
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-13 01:34:18 -0300

    Update UserService.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/service/UserService.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/service/UserService.java
@@ -1,5 +1,14 @@
 package com.enzo.yxzapp.service;
 
+import com.enzo.yxzapp.dto.common.PageResponse;
+import com.enzo.yxzapp.dto.user.CreateUserRequest;
+import com.enzo.yxzapp.dto.user.UpdateUserRequest;
+import com.enzo.yxzapp.dto.user.UserResponse;
+import org.springframework.data.domain.Pageable;
+
 public interface UserService {
-    
+     UserResponse create(CreateUserRequest req);
+     UserResponse update (Long id, UpdateUserRequest req);
+     PageResponse<UserResponse> list(Pageable pageable);
+     UserResponse getById(Long id);
 }
