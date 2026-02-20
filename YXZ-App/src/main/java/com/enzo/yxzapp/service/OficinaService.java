637824db98ddd6942bf2497524338abb23969058
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-19 22:55:37 -0300

    Update OficinaService.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/service/OficinaService.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/service/OficinaService.java
@@ -1,4 +1,11 @@
 package com.enzo.yxzapp.service;
 
+import com.enzo.yxzapp.dto.oficina.CreateOficinaRequest;
+import com.enzo.yxzapp.dto.oficina.OficinaResponse;
+import com.enzo.yxzapp.dto.oficina.UpdateViaModalRequest;
+
+
 public interface OficinaService {
+    OficinaResponse create(CreateOficinaRequest req);
+    OficinaResponse update(Long id, UpdateViaModalRequest req);
 }
