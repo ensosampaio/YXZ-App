1503cffce2a1ec9166721ef1379dc46133cb0d80
Enzo Sampaio <enzosenzos9@gmail.com>
2026-02-19 22:53:39 -0300

    Update OficinaController.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/controller/OficinaController.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/controller/OficinaController.java
@@ -6,9 +6,11 @@ import com.enzo.yxzapp.dto.oficina.OficinaResponse;
 import com.enzo.yxzapp.model.Oficina;
 import com.enzo.yxzapp.service.OficinaService;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class OficinaController {
     private final OficinaService oficinaService;
 
